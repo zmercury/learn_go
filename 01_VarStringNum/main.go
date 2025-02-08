6b8c8f0af40a23eb2391a3b23277aa9d36f7dd25
zmercury <messiniki@outlook.com>
2025-02-08 13:00:28 +0545

    Update

---
+++ 01_VarStringNum/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	//string
+	var nameOne string = "One"
+	var nameTwo = "Two"
+	var nameThree string
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameThree = "One"
+	nameTwo = "Two"
+	nameOne = "Three"
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameFour := "Four"
+
+	fmt.Println(nameFour)
+
+	//numbers
+
+	//int
+
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	//bits and memory
+
+}
